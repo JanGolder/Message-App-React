09eda6ff340106aff9b9ba161dc4bdba039e927e
Jan Golder <kontakt.jangolder@gmail.com>
2023-07-07 06:51:31 +0200

    add loader

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,52 +1,62 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 import classes from "./PostList.module.css";
 
-const PostList = ({isPosting,onStopPosting}) => {
+const PostList = ({ isPosting, onStopPosting }) => {
+  const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
-const [posts, setPosts]=useState([]);
-
-useEffect(()=>{
-
-  async function fetchPosts(){
-    const response = await fetch('http://localhost:8080/posts');
-    const resData = await response.json();
-    setPosts(resData.posts);
-  }
-  fetchPosts();
-  // fetch('http://localhost:8080/posts').then(response=> response.json()).then(data=>setPosts(data.posts));
-},[])
-
-
-function addPostHandler(postData){
-  fetch('http://localhost:8080/posts',{
-    method: 'POST',
-    body: JSON.stringify(postData),
-    headers: {
-      'Content-Type': 'application/json'
+  useEffect(() => {
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+      setIsFetching(false);
     }
-  })
-  setPosts((prevPosts)=>[postData, ...prevPosts]);
-}
+    fetchPosts();
+    // fetch('http://localhost:8080/posts').then(response=> response.json()).then(data=>setPosts(data.posts));
+  }, []);
 
-console.log(posts)
+  function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setPosts((prevPosts) => [postData, ...prevPosts]);
+  }
+
+  console.log(posts);
   return (
     <>
-    {isPosting && <Modal onClose={onStopPosting}>
-        <NewPost
-          onCancel={onStopPosting}
-          onAddPost={addPostHandler}
-        />
-      </Modal>}
-      {posts.length >0 && <ul className={classes.posts}>
-        {posts.map((post)=><Post key={post.body} author={post.author} body={post.body}/>)}
-      </ul>}
-      {posts.length === 0 && <div style={{textAlign: 'center', color: 'white'}}>
-        <h2>There are no posts yet.</h2>
-        <p>Start adding some!</p>
-        </div>}
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
+        </Modal>
+      )}
+      {!isFetching && posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
+      {!isFetching && posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading posts...</p>
+        </div>
+      )}
     </>
   );
 };
