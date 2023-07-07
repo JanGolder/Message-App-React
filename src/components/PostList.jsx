0151a70699824f2253ba87fbde1dc674d379c813
Jan Golder <kontakt.jangolder@gmail.com>
2023-07-07 19:09:34 +0200

    add routing

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,10 +1,8 @@
 import { useState, useEffect } from "react";
 import Post from "./Post";
-import NewPost from "./NewPost";
-import Modal from "./Modal";
 import classes from "./PostList.module.css";
 
-const PostList = ({ isPosting, onStopPosting }) => {
+const PostList = () => {
   const [posts, setPosts] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
 
@@ -34,11 +32,6 @@ const PostList = ({ isPosting, onStopPosting }) => {
   console.log(posts);
   return (
     <>
-      {isPosting && (
-        <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
-        </Modal>
-      )}
       {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
---
+++ src/routes/Posts.jsx
@@ -0,0 +1,20 @@
+import {Outlet} from 'react-router-dom';
+
+import PostList from "../components/PostList";
+
+
+function Posts() {
+
+
+  return (
+    <>
+    <Outlet/>
+    <main>
+      <PostList/>
+    </main>    
+    </>
+
+  );
+}
+
+export default Posts;
