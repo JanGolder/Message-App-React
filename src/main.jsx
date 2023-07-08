e5349c4926e36eba82c41a795003470a52ac8bd1
Jan Golder <kontakt.jangolder@gmail.com>
2023-07-08 19:03:07 +0200

    add post detail func by routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import Posts from "./routes/Posts";
+import Posts, { loader as postsLoaders } from "./routes/Posts";
 import "./index.css";
-import NewPost from "./routes/NewPost";
+import NewPost, { action as newPostAction } from "./routes/NewPost";
+import PostDetails, {loader as postDetailsLoader} from './routes/PostDetails';
 import RootLayout from "./routes/RootLayout";
 
 const router = createBrowserRouter([
@@ -15,7 +16,11 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Posts />,
-        children: [{ path: "/create-post", element: <NewPost /> }],
+        loader: postsLoaders,
+        children: [
+          { path: "/create-post", element: <NewPost />, action: newPostAction },
+          {path:'/:postId', element: <PostDetails/>, loader: postDetailsLoader}
+        ],
       },
     ],
   },
